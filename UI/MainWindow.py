a7f7d1fb02734e339873ea0e1a79b37bf82f85fd
Martin Schreurs <martin.schreurs@netcologne.de>
2023-04-27 21:40:11 +0200

    handle recipes

--- UI/MainWindow.py
+++ UI/MainWindow.py
@@ -8,7 +8,10 @@ from PyQt5.QtWidgets import QMainWindow, QHBoxLayout, QVBoxLayout, QGridLayout, 
 from PyQt5.QtWidgets import QLineEdit, QPushButton, QListWidget, QListWidgetItem, QTextEdit, QLabel
 from UI.RawTypesWindow import RawTypesWindow
 
+from database import add as a
 from database import select as s
+from database import update as u
+from database import delete as d
 
 
 class Colors:
@@ -37,65 +40,94 @@ class RawTypeItem(QListWidgetItem):
             self.setBackground(Colors.white)
 
 
+class RecipeItem(QListWidgetItem):
+    def __init__(self, ID: int, title: str, description: str) -> None:
+        super().__init__()
+        self.ID: int = ID
+        self.title: str = title
+        self.description: str = description
+
+        self.set_title(self.title)
+
+    def set_title(self, new_title: str) -> None:
+        self.title = new_title
+        self.setText(self.title)
+
+
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
 
         self._init_UI()
         self._ini_layout()
+        self._init_data()
 
         self._raw_types_window: RawTypesWindow | None = None
 
-        self._load_raw_types()
-
+    # init
     def _init_UI(self) -> None:
         self.setWindowTitle("Cooking Book - Purpur Tentakel")
 
         # left
         self._recipe_label: QLabel = QLabel("recipes:")
-        self._recipe_search: QLineEdit = QLineEdit()
-        self._recipe_search.setPlaceholderText("recipe search")
-        self._clear_recipe_search: QPushButton = QPushButton("clear")
+        self._recipe_search_le: QLineEdit = QLineEdit()
+        self._recipe_search_le.setPlaceholderText("recipe search")
+        self._recipe_search_le.textChanged.connect(self._chanced_recipe_search)
+        self._clear_recipe_search_btn: QPushButton = QPushButton("clear")
+        self._clear_recipe_search_btn.setEnabled(False)
+        self._clear_recipe_search_btn.clicked.connect(self._clear_recipe_search)
         self._delete_recipe_btn: QPushButton = QPushButton("delete")
-        self._cancel_recipe_btn: QPushButton = QPushButton("cancel")
+        self._delete_recipe_btn.clicked.connect(self._delete_recipe)
         self._add_recipe_btn: QPushButton = QPushButton("add")
-        self._recipes: QListWidget = QListWidget()
+        self._add_recipe_btn.clicked.connect(self._add_recipe)
+        self._recipes_list: QListWidget = QListWidget()
+        self._recipes_list.currentItemChanged.connect(self._chanced_recipe)
+        self._recipes_list.itemClicked.connect(self._clicked_recipe)
 
         # middle
         self._title_label: QLabel = QLabel("title:")
-        self._title: QLineEdit = QLineEdit()
-        self._title.setPlaceholderText("title")
+        self._title_le: QLineEdit = QLineEdit()
+        self._title_le.setPlaceholderText("title")
+        self._title_le.textChanged.connect(self._chanced_title)
+        self._title_le.returnPressed.connect(self._clicked_save_title)
         self._save_title_btn: QPushButton = QPushButton("save")
+        self._save_title_btn.setEnabled(False)
+        self._save_title_btn.clicked.connect(self._clicked_save_title)
         self._types_label: QLabel = QLabel("types:")
-        self._types_search: QLineEdit = QLineEdit()
-        self._types_search.setPlaceholderText("types search")
-        self._types_search.textChanged.connect(self._chanced_text_type_search)
-        self._clear_types_search: QPushButton = QPushButton("clear")
-        self._clear_types_search.clicked.connect(self._clicked_clear_type_search)
-        self._clear_types_search.setEnabled(False)
-        self._types_button: QPushButton = QPushButton("types")
-        self._types_button.clicked.connect(self._set_raw_type_window)
-        self._types: QListWidget = QListWidget()
+        self._types_search_le: QLineEdit = QLineEdit()
+        self._types_search_le.setPlaceholderText("types search")
+        self._types_search_le.textChanged.connect(self._chanced_text_type_search)
+        self._clear_types_search_btn: QPushButton = QPushButton("clear")
+        self._clear_types_search_btn.clicked.connect(self._clear_type_search)
+        self._clear_types_search_btn.setEnabled(False)
+        self._types_btn: QPushButton = QPushButton("types")
+        self._types_btn.clicked.connect(self._set_raw_type_window)
+        self._types_list: QListWidget = QListWidget()
 
         self._ingredient_label: QLabel = QLabel("ingredients:")
-        self._amount: QLineEdit = QLineEdit()
-        self._amount.setPlaceholderText("amount")
-        self._unit: QLineEdit = QLineEdit()
-        self._unit.setPlaceholderText("unit")
-        self._ingredient: QLineEdit = QLineEdit()
-        self._ingredient.setPlaceholderText("ingredient")
+        self._amount_le: QLineEdit = QLineEdit()
+        self._amount_le.setPlaceholderText("amount")
+        self._unit_le: QLineEdit = QLineEdit()
+        self._unit_le.setPlaceholderText("unit")
+        self._ingredient_le: QLineEdit = QLineEdit()
+        self._ingredient_le.setPlaceholderText("ingredient")
         self._delete_ingredient_btn: QPushButton = QPushButton("delete")
         self._cancel_ingredient_btn: QPushButton = QPushButton("cancel")
-        self._add_ingredient_btn: QPushButton = QPushButton("add")
-        self._ingredients: QListWidget = QListWidget()
+        self._add_ingredient_btn: QPushButton = QPushButton("add / update")
+        self._ingredients_list: QListWidget = QListWidget()
 
         # right
         self._recipe_entry_label: QLabel = QLabel("recipe:")
         self._save_recipe_text_btn: QPushButton = QPushButton("save")
-        self._recipe_entry: QTextEdit = QTextEdit()
-        self._recipe_entry.setPlaceholderText("enter the recipe description")
+        self._save_recipe_text_btn.setEnabled(False)
+        self._save_recipe_text_btn.clicked.connect(self._clicked_save_description)
+        self._recipe_entry_te: QTextEdit = QTextEdit()
+        self._recipe_entry_te.setPlaceholderText("enter the recipe description")
+        self._recipe_entry_te.textChanged.connect(self._chanced_description)
 
         self._global_save_btn: QPushButton = QPushButton("save all")
+        self._global_save_btn.setEnabled(False)
+        self._export_btn: QPushButton = QPushButton("export")
 
     def _ini_layout(self) -> None:
         # left
@@ -103,61 +135,65 @@ class MainWindow(QMainWindow):
         left_v_box.addWidget(self._recipe_label)
 
         left_h_box_1: QHBoxLayout = QHBoxLayout()
-        left_h_box_1.addWidget(self._recipe_search)
-        left_h_box_1.addWidget(self._clear_recipe_search)
+        left_h_box_1.addWidget(self._recipe_search_le)
+        left_h_box_1.addWidget(self._clear_recipe_search_btn)
 
         left_v_box.addLayout(left_h_box_1)
 
         left_h_box_2: QHBoxLayout = QHBoxLayout()
         left_h_box_2.addWidget(self._delete_recipe_btn)
-        left_h_box_2.addWidget(self._cancel_recipe_btn)
         left_h_box_2.addWidget(self._add_recipe_btn)
 
         left_v_box.addLayout(left_h_box_2)
-        left_v_box.addWidget(self._recipes)
+        left_v_box.addWidget(self._recipes_list)
 
         # mid
         mid_v_box: QVBoxLayout = QVBoxLayout()
         mid_v_box.addWidget(self._title_label)
 
         mid_h_box_1: QHBoxLayout = QHBoxLayout()
-        mid_h_box_1.addWidget(self._title)
+        mid_h_box_1.addWidget(self._title_le)
         mid_h_box_1.addWidget(self._save_title_btn)
 
         mid_v_box.addLayout(mid_h_box_1)
         mid_v_box.addWidget(self._types_label)
 
         mid_h_box_2: QHBoxLayout = QHBoxLayout()
-        mid_h_box_2.addWidget(self._types_search)
-        mid_h_box_2.addWidget(self._clear_types_search)
-        mid_h_box_2.addWidget(self._types_button)
+        mid_h_box_2.addWidget(self._types_search_le)
+        mid_h_box_2.addWidget(self._clear_types_search_btn)
+        mid_h_box_2.addWidget(self._types_btn)
 
         mid_v_box.addLayout(mid_h_box_2)
-        mid_v_box.addWidget(self._types)
+        mid_v_box.addWidget(self._types_list)
         mid_v_box.addWidget(self._ingredient_label)
 
         mid_bottom_grid: QGridLayout = QGridLayout()
-        mid_bottom_grid.addWidget(self._amount, 0, 0)
-        mid_bottom_grid.addWidget(self._unit, 0, 1)
-        mid_bottom_grid.addWidget(self._ingredient, 0, 2)
+        mid_bottom_grid.addWidget(self._amount_le, 0, 0)
+        mid_bottom_grid.addWidget(self._unit_le, 0, 1)
+        mid_bottom_grid.addWidget(self._ingredient_le, 0, 2)
         mid_bottom_grid.addWidget(self._delete_ingredient_btn, 1, 0)
         mid_bottom_grid.addWidget(self._cancel_ingredient_btn, 1, 1)
         mid_bottom_grid.addWidget(self._add_ingredient_btn, 1, 2)
 
         mid_v_box.addLayout(mid_bottom_grid)
-        mid_v_box.addWidget(self._ingredients)
+        mid_v_box.addWidget(self._ingredients_list)
 
         # right
         right_v_box: QVBoxLayout = QVBoxLayout()
 
-        right_h_box: QHBoxLayout = QHBoxLayout()
-        right_h_box.addWidget(self._recipe_entry_label)
-        right_h_box.addStretch()
-        right_h_box.addWidget(self._save_recipe_text_btn)
+        right_h_box_1: QHBoxLayout = QHBoxLayout()
+        right_h_box_1.addWidget(self._recipe_entry_label)
+        right_h_box_1.addStretch()
+        right_h_box_1.addWidget(self._save_recipe_text_btn)
 
-        right_v_box.addLayout(right_h_box)
-        right_v_box.addWidget(self._recipe_entry)
-        right_v_box.addWidget(self._global_save_btn)
+        right_v_box.addLayout(right_h_box_1)
+        right_v_box.addWidget(self._recipe_entry_te)
+
+        right_h_box2: QHBoxLayout = QHBoxLayout()
+        right_h_box2.addWidget(self._global_save_btn)
+        right_h_box2.addWidget(self._export_btn)
+
+        right_v_box.addLayout(right_h_box2)
 
         # global
         global_h_box: QVBoxLayout = QHBoxLayout()
@@ -173,38 +209,182 @@ class MainWindow(QMainWindow):
         self.showMaximized()
         self.show()
 
+    def _init_data(self) -> None:
+        self._load_recipes()
+        self._load_raw_types()
+
+        self._check_valid_recipe()
+        self._chanced_recipe()
+
+    # check
+    def _check_valid_recipe(self) -> None:
+        count: int = self._recipes_list.count()
+        if count <= 0:
+            self._add_recipe()
+        else:
+            self._recipes_list.setCurrentRow(0)
+
+    # add
+    def _add_recipe(self) -> None:
+        new_name: str = f"new recipe {self._recipes_list.count() + 1}"
+        result = a.add.add_recipe(new_name, "description")
+        if not result.valid:
+            self._display_message(result.entry)
+            return
+
+        recipe: RecipeItem = RecipeItem(result.entry, new_name, "description")
+        self._recipes_list.addItem(recipe)
+        self._recipes_list.setCurrentItem(recipe)
+
+    # update
+    def _update_recipe(self) -> bool:
+        current_recipe: RecipeItem = self._recipes_list.currentItem()
+        if not current_recipe:
+            self._display_message("no selected recipe to update")
+            return False
+
+        result = u.update.update_recipe_by_ID(current_recipe.ID, current_recipe.title, current_recipe.description)
+        if not result.valid:
+            self._display_message(result.entry)
+            return False
+
+        return True
+
+    # delete
+    def _delete_recipe(self) -> None:
+        current_item: RecipeItem = self._recipes_list.currentItem()
+        if not current_item:
+            self._display_message("no selected recipe to delete")
+            return
+        result = d.delete.delete_recipe_by_ID(current_item.ID)
+
+        if not result.valid:
+            self._display_message(result.entry)
+            return
+
+        self._recipes_list.takeItem(self._recipes_list.row(current_item))
+        self._check_valid_recipe()
+
+    # load
     def _load_raw_types(self) -> None:
         result = s.select.select_all_raw_types()
         if not result.valid:
             self._display_message(result.entry)
             return
 
-        self._types.clear()
+        self._types_list.clear()
         for ID, entry in result.entry:
             item: RawTypeItem = RawTypeItem(ID, entry, False)
-            self._types.addItem(item)
+            self._types_list.addItem(item)
 
-    def _clicked_clear_type_search(self) -> None:
-        self._types_search.clear()
+    def _load_recipes(self) -> None:
+        result = s.select.select_all_recipes()
+        if not result.valid:
+            self._display_message(result.entry)
+            return
+
+        self._recipes_list.clear()
+        for ID, title, description in result.entry:
+            recipe: RecipeItem = RecipeItem(ID, title, description)
+            self._recipes_list.addItem(recipe)
+
+    # clear
+    def _clear_type_search(self) -> None:
+        self._types_search_le.clear()
+
+    def _clear_recipe_search(self) -> None:
+        self._recipe_search_le.clear()
+
+    # clicked
+    def _clicked_recipe(self) -> None:
+        current_recipe: RecipeItem = self._recipes_list.currentItem()
+        if not current_recipe:
+            self._display_message("no selected recipe for selecting recipe")
+            return
+
+        self._title_le.setText(current_recipe.title)
+        self._recipe_entry_te.setText(current_recipe.description)
+
+    def _clicked_save_title(self) -> None:
+        current_recipe: RecipeItem = self._recipes_list.currentItem()
+        if not current_recipe:
+            self._display_message("no current recipe for saving title")
+            return
+
+        old_title: str = current_recipe.title
+        current_recipe.set_title(self._title_le.text())
+        if not self._update_recipe():
+            current_recipe.set_title(old_title)
+            return
+        self._save_title_btn.setEnabled(False)
+        self._recipes_list.sortItems()
+
+    def _clicked_save_description(self) -> None:
+        current_recipe: RecipeItem = self._recipes_list.currentItem()
+        if not current_recipe:
+            self._display_message("no selected recipe for saving description")
+            return
+
+        old_text: str = current_recipe.description
+        current_recipe.description = self._recipe_entry_te.toPlainText()
+        if not self._update_recipe():
+            current_recipe.description = old_text
+            return
+        self._save_recipe_text_btn.setEnabled(False)
+
+    # chanced
+    def _chanced_recipe(self) -> None:
+        current_item: RecipeItem = self._recipes_list.currentItem()
+        if not current_item:
+            return
+        self._clicked_recipe()
+
+    def _chanced_recipe_search(self) -> None:
+        current_text: str = self._recipe_search_le.text().strip()
+        self._clear_recipe_search_btn.setEnabled(len(current_text) != 0)
+
+        for i in range(self._recipes_list.count()):
+            current_item: RecipeItem = self._recipes_list.item(i)
+            found: bool = current_text.lower() in current_item.title.lower()
+            current_item.setHidden(not found)
+
+    def _chanced_title(self) -> None:
+        current_text: str = self._title_le.text()
+        current_recipe: RecipeItem = self._recipes_list.currentItem()
+        if not current_recipe:
+            self._display_message("no selected recipe for title chance")
+            return
+
+        self._save_title_btn.setEnabled(current_text != current_recipe.title)
+
+    def _chanced_description(self) -> None:
+        current_recipe: RecipeItem = self._recipes_list.currentItem()
+        if not current_recipe:
+            self._display_message("no selected recipe for description chance")
+            return
+        current_text: str = self._recipe_entry_te.toPlainText()
+        self._save_recipe_text_btn.setEnabled(current_recipe.description != current_text)
 
     def _chanced_text_type_search(self) -> None:
-        text: str = self._types_search.text().strip()
-        self._clear_types_search.setEnabled(len(text) != 0)
+        text: str = self._types_search_le.text().strip()
+        self._clear_types_search_btn.setEnabled(len(text) != 0)
         self._display_raw_types()
 
     def _display_raw_types(self) -> None:
         pass
 
+    # raw type window
     def _set_raw_type_window(self) -> None:
         self.window().setEnabled(False)
         self._raw_types_window = RawTypesWindow(self._raw_types_callback)
 
     def _raw_types_callback(self) -> None:
         self._load_raw_types()
-        self._types_search.clear()
+        self._types_search_le.clear()
         self._raw_types_window = None
         self.window().setEnabled(True)
 
+    # statics
     @staticmethod
     def _display_message(message: str) -> None:
         msg: QMessageBox = QMessageBox()
