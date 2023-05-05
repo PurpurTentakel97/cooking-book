c098865411dfcbfcfe409a0b680d8c791e495aa3
Martin Schreurs <martin.schreurs@netcologne.de>
2023-05-05 23:46:02 +0200

    first pdf

--- pdf/pdfHandler.py
+++ pdf/pdfHandler.py
@@ -4,5 +4,57 @@
 # 04.05.2023
 #
 
-def export_recipe(ID: int, open_pdf: bool) -> None:
-    print(f"TODO: export recipe with ID -> {ID} | should open -> {open_pdf}")
+import os
+from pdf import recipePDF as rP
+from helper import log, dirs, return_message
+
+_recipe_pdf: rP.RecipePDF | None = None
+
+
+def _check_recipe_pdf() -> None:
+    global _recipe_pdf
+    if not _recipe_pdf:
+        _recipe_pdf = rP.RecipePDF()
+
+
+def _get_export_file(my_path: str) -> return_message.ReturnMessage:
+    if not my_path:
+        log.message(log.LogType.ERROR, "pdfHandler.py", "export_recipe()", "no path provided")
+        return return_message.ReturnMessageNone(False)
+
+    _, file_name = os.path.split(my_path)
+    result = file_name.split('.')
+
+    if len(result) == 0:
+        log.message(log.LogType.ERROR, "pdfHandler.py", "export_recipe()", "no arguments for filename")
+        return return_message.ReturnMessageNone(False)
+    elif len(result) == 1:
+        file_name = file_name + dirs.FileType.EXPORT.value
+    elif len(result) == 2:
+        name, _type = result
+        _type = '.' + _type
+        if not _type == dirs.FileType.EXPORT.value:
+            log.message(log.LogType.ERROR, "pdfHandler.py", "export_recipe()", "wrong ending for recipe pdf")
+            return return_message.ReturnMessageNone(False)
+    if len(result) > 2:
+        log.message(log.LogType.ERROR, "pdfHandler.py", "export_recipe()", "too many arguments in filename")
+        return return_message.ReturnMessageNone(False)
+
+    return return_message.ReturnMessageStr(file_name, True)
+
+
+def export_recipe(ID: int, my_path: str) -> bool:
+    _check_recipe_pdf()
+
+    result = _get_export_file(my_path)
+    if not result.valid:
+        return False
+    file_name: str = result.entry
+    dirs.check_and_make_dir(dirs.DirType.EXPORT)
+
+    return _recipe_pdf.export(dirs.get_dir_from_file(dirs.FileType.EXPORT), file_name, ID)
+
+
+def open_pdf(my_path: str) -> bool:
+    print(f"TODO open pdf at -> {my_path}")
+    return False
---
+++ pdf/recipePDF.py
@@ -0,0 +1,161 @@
+#
+# Purpur Tentakel
+# Cocking Book
+# 05.05.2023
+#
+
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.units import cm
+from reportlab.pdfgen import canvas
+from reportlab.lib.enums import TA_RIGHT, TA_CENTER
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer
+
+from helper import log
+from database import select as s
+
+
+class RecipePDF:
+    def __init__(self) -> None:
+        self.dir_name: str = str()
+        self.file_name: str = str()
+        self.style_sheet = getSampleStyleSheet()
+        self.custom_styles: dict = dict()
+        self._add_styles()
+
+    def _add_styles(self) -> None:
+        self.custom_styles['CustomTitle'] = (ParagraphStyle(name='CustomTitle', parent=self.style_sheet['Title'],
+                                                            fontSize=35, leading=1.2 * cm))
+        self.custom_styles['CustomHeading'] = (ParagraphStyle(name='CustomHeading', parent=self.style_sheet['Heading1'],
+                                                              fontSize=15))
+        self.custom_styles['CustomBodyTextLeft'] = (ParagraphStyle(name='CustomBodyTextRight',
+                                                                   parent=self.style_sheet['BodyText'], fontSize=10,
+                                                                   leading=0.2 * cm))
+        self.custom_styles['CustomBodyTextRight'] = (ParagraphStyle(name='CustomBodyTextRight',
+                                                                    parent=self.style_sheet['BodyText'], fontSize=10,
+                                                                    alignment=TA_RIGHT))
+        self.custom_styles['CustomBodyTextCenter'] = (ParagraphStyle(name='CustomBodyTextCenter',
+                                                                     parent=self.style_sheet['BodyText'], fontSize=10,
+                                                                     alignment=TA_CENTER))
+        self.custom_styles['CustomBodyTextSmall'] = (ParagraphStyle(name='CustomBodyTextSmall',
+                                                                    parent=self.style_sheet['BodyText'], fontSize=6,
+                                                                    leading=0.23 * cm))
+        self.custom_styles['CustomBodyTextSmallCenter'] = (ParagraphStyle(name='CustomBodyTextSmallCenter',
+                                                                          parent=self.style_sheet['BodyText'],
+                                                                          fontSize=6, leading=0.23 * cm,
+                                                                          alignment=TA_CENTER))
+        self.custom_styles['CustomCenterHeading3'] = (ParagraphStyle(name='CustomCenterHeading3',
+                                                                     parent=self.style_sheet['Heading3'],
+                                                                     alignment=TA_CENTER))
+
+    def export(self, e_dir: str, file_name: str, ID: int) -> bool:
+        self.dir_name = e_dir
+        self.file_name = file_name
+
+        doc: SimpleDocTemplate = self._get_doc()
+        recipe_result = s.select.select_recipe_by_ID(ID)
+        if not recipe_result.valid:
+            log.message(log.LogType.ERROR, "recipePDF.py", "self.export()",
+                        f"failed to load recipe data with ID -> {ID}")
+            return False
+
+        elements: list = list()
+        elements.extend(self._get_headline(recipe_result.entry))
+        elements.extend(self._get_tags(ID))
+        elements.extend(self._get_description(recipe_result.entry))
+
+        return self._export(doc=doc, elements=elements)
+
+    def _get_headline(self, recipe_data: list) -> list:
+        _, title, _ = recipe_data
+        elements: list = list()
+
+        elements.append(Paragraph(title, self.custom_styles["CustomTitle"]))
+        elements.append(Spacer(width=0, height=1 * cm))
+
+        return elements
+
+    def _get_tags(self, ID: int) -> list:
+        tag_result = s.select.select_type_by_recipe_ID(ID)
+        if not tag_result.valid:
+            log.message(log.LogType.ERROR, "recipePDF.py", "self._get_tags()",
+                        f"could not load tags from recipe id -> {ID}")
+            return list()
+
+        elements: list = list()
+        elements.append(Paragraph("Tags:", self.custom_styles['CustomHeading']))
+
+        if len(tag_result.entry) == 0:
+            elements.append(Paragraph("no tags", self.custom_styles['CustomBodyTextLeft']))
+        else:
+            for _, _, raw_type_ID_s in tag_result.entry:
+                raw_type_result = s.select.select_raw_type_by_ID(raw_type_ID_s)
+                if not raw_type_result.valid:
+                    log.message(log.LogType.ERROR, "recipePDF.py", "self._get_tags()",
+                                f"could not load tag from tag ID -> {ID}")
+                    continue
+                elements.append(Paragraph(f"- {raw_type_result.entry}", self.custom_styles['CustomBodyTextLeft']))
+
+        elements.append(Spacer(width=0, height=1 * cm))
+        return elements
+
+    def _get_ingredients(self, ID: int) -> list:
+        pass
+
+    def _get_description(self, recipe_data: list) -> list:
+        _, _, description = recipe_data
+        description: str
+        description = description.replace("\n", "<br/>")
+        elements: list = list()
+
+        elements.append(Paragraph("Description:", self.custom_styles["CustomHeading"]))
+        elements.append(Paragraph(description, self.style_sheet['BodyText']))
+
+        return elements
+
+    def _paragraph(self, value) -> Paragraph:
+        if isinstance(value, list):
+            return Paragraph(str(value[0]) + ": " + str("---" if not value[1] else value[1]),
+                             self.style_sheet["BodyText"])
+        else:
+            return Paragraph(str("---" if not value else value), self.style_sheet["BodyText"])
+
+    def _get_doc(self) -> SimpleDocTemplate:
+        return SimpleDocTemplate(f"{self.dir_name}/{self.file_name}", showBoundary=0, pagesize=A4, rightMargin=1.5 * cm,
+                                 leftMargin=1.5 * cm, topMargin=1.5 * cm, bottomMargin=1.5 * cm)
+
+    def _export(self, doc: SimpleDocTemplate, elements: list, numbered: bool = True) -> bool:
+        try:
+            if numbered:
+                doc.build(elements, canvasmaker=NumberedCanvas)
+            else:
+                doc.build(elements)
+            return True
+        except PermissionError:
+            log.message(log.LogType.ERROR, "recipePDF.py", "self._export()",
+                        f"no premission to export PDF -> {self.dir_name}\\{self.file_name}")
+            return False
+
+
+class NumberedCanvas(canvas.Canvas):
+    def __init__(self, *args, **kwargs) -> None:
+        canvas.Canvas.__init__(self, *args, **kwargs)
+        self._saved_page_states = []
+
+    def showPage(self) -> None:
+        self._saved_page_states.append(dict(self.__dict__))
+        self._startPage()
+
+    def save(self) -> None:
+        """add page info to each page (page x of y)"""
+        num_pages = len(self._saved_page_states)
+        for state in self._saved_page_states:
+            self.__dict__.update(state)
+            self.draw_page_number(num_pages)
+            canvas.Canvas.showPage(self)
+        canvas.Canvas.save(self)
+
+    def draw_page_number(self, page_count) -> None:
+        self.setFont("Helvetica", 10)
+        self.drawRightString(20 * cm, 2 * cm,
+                             "Seite %d von %d" % (self._pageNumber, page_count))
